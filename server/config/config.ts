f4d76ee5248e24bb1c85d62da43866b8a73d6a78
Konrad Winnicki <konrad.winnicki@protonmail.com>
2023-10-31 16:08:48 +0100

    Refactor workflow pipeline

--- server/config/config.ts
+++ server/config/config.ts
@@ -6,31 +6,16 @@ dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 console.log(dotenv.config({ path: path.resolve(process.cwd(), '.env') }))
 
 interface ENV {
-  PAIR_PASSWORD: string | undefined;
   NODE_ENV: string | undefined;
- // PORT: number | undefined;
-  MONGO_URI: string | undefined;
-  DATABASE: string | undefined;
-  JWT_SECRET: string | undefined;
 }
 
 interface Config {
-  PAIR_PASSWORD: string;
   NODE_ENV: string;
- // PORT: number;
- MONGO_URI: string;
-  DATABASE: string;
-  JWT_SECRET: string;
 }
 
 const getConfig = (): ENV => {
   return {
-    PAIR_PASSWORD: process.env.PAIR_PASSWORD,
-    DATABASE: process.env.DATABASE,
     NODE_ENV: process.env.NODE_ENV,
-    //PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
-    MONGO_URI: process.env.MONGO_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
   };
 };
 
